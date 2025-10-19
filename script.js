d847e3437fbad7a7b0d43d3c8ffbd2217a488dac
Rolex Oduor <roduor@gmail.com>
2025-10-19 17:39:50 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,24 +3,32 @@ const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q="
 
 const searchBox = document.querySelector(".search input");
 const searchBtn = document.querySelector(".search button");
-const weathericon = document.querySelector("weather-icon"); 
+const weatherIcon = document.querySelector(".weather-icon");
 
-
-async function checkweather(city) {
+async function checkWeather(city) {
   const response = await fetch(`${apiUrl + city}&appid=${apiKey}`);
+
+  if (response.status == 404) {
+    alert("City not found!");
+    return;
+  }
+
   const data = await response.json();
-
-
-if (data.weather(0).main ==)
-
   console.log(data);
 
   document.querySelector(".city").innerHTML = data.name;
   document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
   document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
   document.querySelector(".wind").innerHTML = data.wind.speed + " Km/h";
+
 }
 
 searchBtn.addEventListener("click", () => {
-  checkweather(searchBox.value);
+  checkWeather(searchBox.value.trim());
+});
+
+searchBox.addEventListener("keypress", (e) => {
+  if (e.key === "Enter") {
+    checkWeather(searchBox.value.trim());
+  }
 });
